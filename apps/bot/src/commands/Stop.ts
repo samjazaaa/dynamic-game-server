f6dcf3a1ef2a8ba57232895886f71f9683081eab
Felix Knoerr <felix.knoerr@gmx.de>
2023-06-24 21:51:39 +0200

    concept for stop command

--- apps/bot/src/commands/Stop.ts
+++ apps/bot/src/commands/Stop.ts
@@ -6,7 +6,16 @@ export const Stop: ExecutableCommand = {
     .setName("stop")
     .setDescription("Stops the currently running game server"),
   run: async (interaction: ChatInputCommandInteraction) => {
-    const content = "Placeholder for stop message...";
+    // TODO notify client that request is processed to prevent timeout?
+
+    // TODO post actual stop message
+    const response = { success: true, game: "Placeholder Craft" };
+
+    let content = response.success
+      ? "Successfully stopped server: " + response.game
+      : "Could not stop game server" + response.game
+      ? ": " + response.game
+      : " because no server was running";
 
     await interaction.reply({
       ephemeral: true,
