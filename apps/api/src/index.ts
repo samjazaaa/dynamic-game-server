61e4e6c5b92ef69e935314edcc1d8676d2b72d4e
Felix Knoerr <felix.knoerr@gmx.de>
2023-06-24 23:01:03 +0200

    api placeholders

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,38 @@
 import express from "express";
 
 const app = express();
+const apiKey = process.env["API_KEY"];
+
+console.log(apiKey);
+
+// require api key for any request
+app.use((req, res, next) => {
+  const sentApiKey = req.get("API-Key");
+  if (!sentApiKey || sentApiKey !== apiKey) {
+    res.status(401).json({ error: "unauthorised" });
+  } else {
+    next();
+  }
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).json({ message: "Hello World!" });
+});
+
+app.get("/status", (req, res) => {
+  res.status(200).json({ message: "status placeholder" });
+});
+
+app.get("/available", (req, res) => {
+  res.status(200).json({ message: "available placeholder" });
+});
+
+app.get("/start", (req, res) => {
+  res.status(200).json({ message: "start placeholder" });
+});
+
+app.get("/stop", (req, res) => {
+  res.status(200).json({ message: "stop placeholder" });
 });
 
 app.listen(3001, () => {
