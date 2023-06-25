29d5e20f9d760f73cb6d54f49c397a4a2550914a
Felix Knoerr <felix.knoerr@gmx.de>
2023-06-25 15:51:11 +0200

    env var check

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
 // api key for communication with bot
 const apiKey = process.env["API_KEY"];
+if (!apiKey) {
+  console.log(
+    "No API key specified. Please set the environment variable API_KEY"
+  );
+  process.exit(1);
+}
 
 // save all actual server data for the available game servers under this path
 const serverDataPath = process.env["SERVER_DATA"];
+if (!serverDataPath || !fs.existsSync(serverDataPath)) {
+  console.log(
+    "No valid data path specified. Please set the environment variable SERVER_DATA and make sure the directory exists."
+  );
+  process.exit(1);
+}
 
-// TODO check if all required env variables are provided and accessible
+// TODO define type / interface for available game server => parse from yaml in folder?
+// TODO scan all available game server templates
 
 // require api key for any request
 app.use((req, res, next) => {
